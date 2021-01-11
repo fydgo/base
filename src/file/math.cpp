116bc4ff41db0d556581b4cbffd6bfe085c19be2
Longhai Quan <longhai.quan@csdental.com>
2021-01-11 16:58:55 +0800

    create base module

---
+++ src/file/math.cpp
@@ -0,0 +1,9 @@
+#include "file/math.h"
+
+namespace base {
+
+int Sum(const int a, const int b) {
+  return a + b;
+}
+
+}
---
+++ src/file/math.h
@@ -0,0 +1,10 @@
+#ifndef BASE_MATH_H_
+#define BASE_MATH_H_
+
+namespace base {
+
+int Sum(const int a, const int b);
+
+}
+
+#endif // BASE_MATH_H_
